0c634f7ecd03756c4e1dc566959688c2f84e98bc
linhsuanyu <wayne315315@gmail.com>
2024-11-03 19:16:53 +0800

    Fine tune animation paras

--- visual.py
+++ visual.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-from r3 import coords
-from r3 import xp, xn, yp, yn, zp, zn
+from r3 import coords, rs
+from r3 import xpp, xpn, x0p, x0n, xnp, xnn, ypp, ypn, y0p, y0n, ynp, ynn, zpp, zpn, z0p, z0n, znp, znn
 
 # prerequisites : sudo apt install ffmpeg
 
@@ -54,6 +54,8 @@ def draw_rotation(ax, r, radius=2.5, theta_bgn=0, theta_end=350):
         return
     # init
     theta = np.linspace((2 * np.pi) * theta_bgn / 360, (2 * np.pi) * theta_end / 360 , theta_end - theta_bgn)
+    if r.orient == -1:
+        theta = -theta
     points = np.zeros((3, len(theta)))
     # assign value
     orient = [[1,2,0], [2,0,1], [0,1,2]]
@@ -66,24 +68,27 @@ def draw_rotation(ax, r, radius=2.5, theta_bgn=0, theta_end=350):
     ax.plot(x[:-1], y[:-1], z[:-1], color="red")
     # draw arrowhead
     ax.quiver(x[-2], y[-2], z[-2], x[-1]-x[-2], y[-1]-y[-2], z[-1]-z[-2], arrow_length_ratio=5, color="red")
+    # label rotation
+    ax.text2D(0.05, 0.95, str(r), color='red', fontsize='large', transform=ax.transAxes)
 
-def animate(i, seq, coords_list, limit=2):
+def animate(i, seq, coords_list, angle_per_frame, limit=2):
+    angle = i * angle_per_frame
     seq += [None]
     ax = plt.gca()
-    if i % 360 == 0:
+    if angle % 360 == 0:
         ax.cla()
         ax.set_xlim(-limit, limit)
         ax.set_ylim(-limit, limit)
         ax.set_zlim(-limit, limit)
         plt.axis("off")
-        r = seq[i // 360]
-        coords = coords_list[i // 360]
+        r = seq[angle // 360]
+        coords = coords_list[angle // 360]
         draw_cube(ax, coords)
         draw_axes(ax)
         draw_rotation(ax, r)
 
-    azim = i % 360
-    elev = (i * 4) % 360
+    azim = angle % 360
+    elev = (angle * 4) % 360
     if 90 < elev <= 270:
         elev = 180 - elev
     elif 270 < elev < 360:
@@ -93,17 +98,17 @@ def animate(i, seq, coords_list, limit=2):
     return fig,
 
 if __name__ == "__main__":
-    seq = [xp, yp] * 1
+    seq = [x0n, x0p, ypn, z0n] * 1
     coords_list = [coords]
     for r in seq:
         coords_list.append(r(coords_list[-1]))
 
-    frames = 360 * (len(seq) + 1)
+    angle_per_frame = 6
+    frames = (360 // angle_per_frame) * (len(seq) + 1)
     interval = 1
-    fps = 90
+    fps = 12
 
-    anim = animation.FuncAnimation(fig, animate, frames=frames, interval=interval, blit=True, fargs=(seq, coords_list))
+    anim = animation.FuncAnimation(fig, animate, frames=frames, interval=interval, blit=True, fargs=(seq, coords_list, angle_per_frame))
+    # mp4 format
     writer = animation.FFMpegWriter(fps=fps)
     anim.save("rubik.mp4", writer=writer)
-    #writer = animation.PillowWriter(fps=fps)
-    #anim.save('rubik.gif',writer=writergif)
